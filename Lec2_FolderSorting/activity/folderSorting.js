eff177da9d768d95d5169d3af5d0c78e6b389495
Lalit <lalitaditya315@gmail.com>
2021-07-14 16:14:05 +0530

    final commit

---
+++ Lec2_FolderSorting/activity/folderSorting.js
@@ -0,0 +1,67 @@
+let fs = require("fs") ;
+let path = require("path") ;
+let extensions = require("./util") ;
+
+let folderPath = "./Downloads" ;
+let extensionFolderPath ;
+
+
+function checkFolder(extension){
+
+    extensionFolderPath = folderPath ;
+    
+    for(let key in extensions){
+        if(extensions[key].indexOf(extension) > -1){
+            extensionFolderPath += "/"+ key ;
+            break ;   
+        }
+    }
+
+    return fs.existsSync(extensionFolderPath) ;
+}
+
+function moveFile(fileName){
+
+    // copy file
+    let srcPath = folderPath+"/"+fileName ;
+    let destPath = extensionFolderPath+"/"+fileName ;
+
+    fs.copyFileSync(srcPath,destPath) ;
+
+    // remove file
+    fs.unlinkSync(srcPath) ;
+}
+
+function createFolder(){
+
+    fs.mkdirSync(extensionFolderPath) ;
+}
+
+function sortFolder(folderPath){
+
+    let contents = fs.readdirSync(folderPath) ;
+
+    //console.log(contents) ; //[".png",".jpg",".doc",.......]
+
+    for(let i = 0 ; i < contents.length ; i++){
+
+        let extension = path.extname(contents[i]);
+         //console.log(extension) ; 
+
+        let hasFolder = checkFolder(extension) ;
+
+        if(hasFolder){
+            // move file
+            moveFile(contents[i]) ;
+        }
+        else{
+            // create folder
+            createFolder() ;
+            //move file
+            moveFile(contents[i]) ;
+        }
+    }
+}
+
+
+sortFolder(folderPath) ;
---
+++ Lec2_FolderSorting/activity/util.js
@@ -0,0 +1,9 @@
+let extensions = {
+    Images : [".jpg",".jpeg",".png",".gif"] ,
+    Documents : [".txt",".pdf",".doc"] ,
+    Audio : [".mp3"] ,
+    Video : [".mkv"] ,
+    Compressed : [".zip"]
+}
+
+module.exports = extensions ;
