6a3c1be1470ab28120eaa5b3e7ee236334db44ed
Lalit <lalitaditya315@gmail.com>
2021-07-15 20:26:11 +0530

    final commit

--- Lec2_FolderSorting/activity/folderSorting.js
+++ Lec2_FolderSorting/activity/folderSorting.js
@@ -10,7 +10,7 @@ let extensionFolderPath ;
 
 function checkFolder(extension , folderPath){
 
-    // .png
+    
     extensionFolderPath = folderPath ;
 
     for(let key in extensions){                 // key ==> Images,Documents,Compressed,Audio,Video
@@ -25,9 +25,9 @@ function checkFolder(extension , folderPath){
 function moveFile(folderPath , fileName){
 
     // copy file from src to dest
-        let src = folderPath+"/"+fileName ;         // Downloads/Misc/def.png
-        let dest = `${extensionFolderPath}/${fileName}` ;
-        fs.copyFileSync(src,dest) ;  // Downloads/Misc/Images
+        let src = `${folderPath}/${fileName}` ;                  // Downloads/Misc/def.png
+        let dest = `${extensionFolderPath}/${fileName}` ;        // Downloads/Misc/Images/def.png
+        fs.copyFileSync(src,dest) ;                             
 
     // delete file at src
 
@@ -42,7 +42,7 @@ function createFolder(){
 function sortFolder(folderPath){
 
     let contents = fs.readdirSync(folderPath) ;     // folderPath = ./Downloads
-    // console.log(contents) ;           // [Audio,Compressed,Documents,Folder1,Folder2,Images,Misc,Video,def.mp3,def.pdf....]                          
+    // console.log(contents) ;                      // [Audio,Compressed,Documents,Folder1,Folder2,Images,Misc,Video,def.mp3,def.pdf....]                          
 
     for(let i = 0 ; i < contents.length ; i++){
 
@@ -51,20 +51,17 @@ function sortFolder(folderPath){
         let isFolder = fs.lstatSync(path).isDirectory() ;
 
         if(isFolder){
-            // console.log("Folder name "+ contents[i]) ;
             let isSorted = false ;
             
             for(let key in extensions){                 // keys ==> Images, Documents , Audio , Video , Compressed
-                
                 if(key == contents[i]){
-                    // console.log("if block entered") ;
                     isSorted = true ;
                     break ;
                 }
             }
 
             if(!isSorted)
-                sortFolder(folderPath+"/"+contents[i]) ;
+                sortFolder(path) ;
         }
         else{
 
@@ -85,54 +82,3 @@ function sortFolder(folderPath){
 
 sortFolder(folderPath) ;
 
-// let contents = fs.readdirSync(folderPath) ;
-
-//     //console.log(contents) ; [".png",".jpg",".doc",.......]
-
-//     for(let i = 0 ; i < contents.length ; i++){
-
-//         let extension = path.extname(contents[i]);
-
-//          //console.log(extension) ; 
-
-//         let hasFolder = checkFolder(extension) ;
-
-//         if(hasFolder){
-//             // move file
-//             moveFile(contents[i]) ;
-//         }
-//         else{
-//             // create folder
-//             createFolder() ;
-//             //move file
-//             moveFile(contents[i]) ;
-//         }
-//     }
-
-
-
-
-
-
-//extensionFolderPath = folderPath ;
-    
-    // for(let key in extensions){
-    //     if(extensions[key].indexOf(extension) > -1){
-    //         extensionFolderPath += "/"+ key ;
-    //         break ;   
-    //     }
-    // }
-
-    // return fs.existsSync(extensionFolderPath) ;
-
-
-    // // copy file
-    // let srcPath = folderPath+"/"+fileName ;
-    // let destPath = extensionFolderPath+"/"+fileName ;
-
-    // fs.copyFileSync(srcPath,destPath) ;
-
-    // // remove file
-    // fs.unlinkSync(srcPath) ;
-
-    // fs.mkdirSync(extensionFolderPath) ;
