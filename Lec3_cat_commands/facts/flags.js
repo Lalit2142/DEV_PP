8fbe746068dc3ff0ea9ec4915c5b92fb38cec430
Lalit <lalitaditya315@gmail.com>
2021-07-15 15:00:22 +0530

    lec3

---
+++ Lec3_cat_commands/facts/flags.js
@@ -0,0 +1,58 @@
+let fs = require("fs") ;
+
+let fileContent = fs.readFileSync("./f1.txt",'utf-8') ;
+
+// console.log(fileContent) ;
+let content = fileContent.split("\r\n") ;
+console.log(content) ;
+
+// -s
+
+function removeExtraLines(data){
+
+    let res = [] ;
+
+    for(let i = 0 ; i < data.length ; i++){
+        if(data[i] != ''){
+            res.push(data[i]) ;
+        }
+    }
+    // console.log(res) ;
+
+    let resString = res.join("\n") ;
+    console.log(resString) ;
+}
+
+// removeExtraLines(content) ;
+
+// -b
+
+function addLineNumbersToNonEmptyLines(data){
+
+    let count = 1 ;
+    for(let i = 0 ; i < data.length ; i++){
+        if(data[i] != ''){
+            data[i] = `${count}. ${data[i]}` ;
+            count++ ;
+        }
+    }
+
+    let resString = data.join("\n") ;
+    console.log(resString) ;
+}
+
+// addLineNumbersToNonEmptyLines(content) ;
+
+// -n
+
+function addLineNumbersToNonAllLines(data){
+
+    for(let i = 0 ; i < data.length ; i++){
+        data[i] = `${i+1}. ${data[i]}` ;
+    }
+
+    let resString = data.join("\n") ;
+    console.log(resString) ;
+}
+
+addLineNumbersToNonAllLines(content) ;
